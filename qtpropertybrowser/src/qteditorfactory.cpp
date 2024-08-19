78c2ffb6568868515bbd2f73e6bbec69bf5353bf
Thomas Beihammer <thomas.beihammer@swarovski.com>
2024-08-19 11:40:09 +0200

    fix(qteditorfactory): implemented QColorDialog - method changes

--- qtpropertybrowser/src/qteditorfactory.cpp
+++ qtpropertybrowser/src/qteditorfactory.cpp
@@ -2293,11 +2293,10 @@ void QtColorEditWidget::setValue(const QColor &c)
 
 void QtColorEditWidget::buttonClicked()
 {
-    bool ok = false;
     QRgb oldRgba = m_color.rgba();
-    QRgb newRgba = QColorDialog::getRgba(oldRgba, &ok, this);
-    if (ok && newRgba != oldRgba) {
-        setValue(QColor::fromRgba(newRgba));
+    QColor newColor = QColorDialog::getColor(QColor(oldRgba), this, QString(), QColorDialog::ShowAlphaChannel);
+    if (newColor.isValid() && newColor.rgba() != oldRgba) {
+        setValue(newColor);
         emit valueChanged(m_color);
     }
 }
